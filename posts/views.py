037c262edf954ca71ab41df89ad508cde84f6b89
Sriram Arvind L <lsriramarvind95@hotmail.com>
2018-01-22 13:52:15 +0530

    Till video 20

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
-def post_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if (form.is_valid()):
+        instance = form.save(commit=False)
+        instance.save()
+
+
+    context = dict()
+    context['form'] = form
+
+    return render(request,"post_form.html",context)
+
+def post_detail(request,id=None): #retrive
+    instance = get_object_or_404(Post,id = id)
+    context = dict()
+    context['title'] = instance.title
+    context['instance'] = instance
+    return render(request,"post_detail.html",context)
+
+def post_list(request): #list items
+
+    context = dict()
+    context['title'] = 'List'
+    context['object_list'] = Post.objects.all()
+    return render(request, "index.html", context)
+    # return HttpResponse("<h1>List</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
